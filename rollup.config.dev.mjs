37b45a75701e7c3d76f3555f9d24934393444d5b
Matthieu Patou <mat@matws.net>
2024-04-04 13:02:32 -0700

    Reformat rollup config files

--- rollup.config.dev.mjs
+++ rollup.config.dev.mjs
@@ -1,53 +1,53 @@
 import { babel } from '@rollup/plugin-babel';
-import resolve  from '@rollup/plugin-node-resolve';
-import commonjs  from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import fs from 'fs';
-import postcss from 'rollup-plugin-postcss'
+import postcss from 'rollup-plugin-postcss';
 import localResolve from 'rollup-plugin-local-resolve';
 import livereload from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 
 export default {
-    input: 'src/index.js',
-    plugins: [
-        json({
-            exclude: [ 'node_modules' ],
-            preferConst: true,
-        }),
-        localResolve(),
-        postcss({ extract: 'dist/cubism-es.css' }),
-        babel(),
-        resolve({
-            module: true,
-            jsnext: true,
-            main: true,
-            extensions: ['.js']
-        }),
-        commonjs(),
-        serve({
-            open: true,
-            verbose: true,
-            contentBase: ['demo', 'dist'],
-            historyApiFallback: false,
-            host: 'localhost',
-            port: 3004
-        }),
-        livereload({
-            watch: 'demo',
-            verbose: false
-        })
-    ],
-    external: ['d3'],
-    output: [
-        {
-            file: 'dist/cubism-es.standalone.js',
-            format: 'umd',
-            name: 'cubism',
-            globals: {
-                d3: 'd3'
-            },
-            sourcemap: true
-        }
-    ]
+  input: 'src/index.js',
+  plugins: [
+    json({
+      exclude: ['node_modules'],
+      preferConst: true,
+    }),
+    localResolve(),
+    postcss({ extract: 'dist/cubism-es.css' }),
+    babel(),
+    resolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      extensions: ['.js'],
+    }),
+    commonjs(),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ['demo', 'dist'],
+      historyApiFallback: false,
+      host: 'localhost',
+      port: 3004,
+    }),
+    livereload({
+      watch: 'demo',
+      verbose: false,
+    }),
+  ],
+  external: ['d3'],
+  output: [
+    {
+      file: 'dist/cubism-es.standalone.js',
+      format: 'umd',
+      name: 'cubism',
+      globals: {
+        d3: 'd3',
+      },
+      sourcemap: true,
+    },
+  ],
 };
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,66 +1,66 @@
 import { babel } from '@rollup/plugin-babel';
-import resolve  from '@rollup/plugin-node-resolve';
-import commonjs  from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import localResolve from 'rollup-plugin-local-resolve';
 import json from '@rollup/plugin-json';
 import fs from 'fs';
 
 // disable circular dependency warning
 
-let data = fs.readFileSync('./package.json', {encoding: 'utf8'})
+let data = fs.readFileSync('./package.json', { encoding: 'utf8' });
 let pkg = JSON.parse(data);
 
 export default {
-    onwarn: function ( message ) {
-        if (message.code === 'CIRCULAR_DEPENDENCY') {
-                return;
-        }
-        console.warn(message);
+  onwarn: function (message) {
+    if (message.code === 'CIRCULAR_DEPENDENCY') {
+      return;
+    }
+    console.warn(message);
+  },
+  input: 'src/index.js',
+  plugins: [
+    json({
+      exclude: ['node_modules'],
+      preferConst: true,
+    }),
+    localResolve(),
+    babel(),
+    resolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      browser: true,
+      extensions: ['.js'],
+    }),
+    commonjs(),
+  ],
+  external: ['d3'],
+  output: [
+    {
+      file: pkg.main,
+      format: 'iife',
+      name: 'cubism',
+      sourcemap: true,
+      globals: {
+        d3: 'd3',
+      },
     },
-    input: 'src/index.js',
-    plugins: [
-        json({
-            exclude: [ 'node_modules' ],
-            preferConst: true,
-        }),
-        localResolve(),
-        babel(),
-        resolve({
-            module: true,
-            jsnext: true,
-            main: true,
-            browser: true,
-            extensions: ['.js']
-        }),
-        commonjs(),
-    ],
-    external: ["d3"],
-    output: [
-        {
-            file: pkg.main,
-            format: 'iife',
-            name: 'cubism',
-            sourcemap: true,
-            globals: {
-                d3: 'd3'
-            },
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            sourcemap: true,
-            globals: {
-                d3: 'd3'
-            },
-        },
-        {
-            file: 'dist/cubism-es.standalone.js',
-            format: 'umd',
-            name: 'cubism',
-            globals: {
-                d3: 'd3'
-            },
-            sourcemap: true
-        }
-    ]
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      globals: {
+        d3: 'd3',
+      },
+    },
+    {
+      file: 'dist/cubism-es.standalone.js',
+      format: 'umd',
+      name: 'cubism',
+      globals: {
+        d3: 'd3',
+      },
+      sourcemap: true,
+    },
+  ],
 };
