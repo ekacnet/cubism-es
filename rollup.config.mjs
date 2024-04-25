c31359f46cca8d93fa090587fde075da5e74836d
Matthieu Patou <mat@matws.net>
2024-04-25 15:43:52 -0700

    Fix rollup config to allow typescript files
    Signed-off-by: Matthieu Patou <mat@matws.net>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import localResolve from 'rollup-plugin-local-resolve';
+import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import fs from 'fs';
 
@@ -30,9 +31,10 @@ export default {
       jsnext: true,
       main: true,
       browser: true,
-      extensions: ['.js'],
+      extensions: ['.js', '.ts'],
     }),
     commonjs(),
+    typescript(),
   ],
   external: ['d3'],
   output: [
